b633849c6dee23118a9f03b371eb793395de6537
William Jeynes <william.jeynes@hotmail.com>
2026-03-15 15:32:59 +0000

    Get to 100% code coverage

--- tests/test_prep.py
+++ tests/test_prep.py
@@ -2,10 +2,18 @@ import unittest
 from prep import strange_function
 
 class MyTestCase(unittest.TestCase):
-    def test_strange_function1(self):
-        self.assertEqual(
-            first=strange_function(1, 2, 3, 4),
-            second='behaviour 3'
-        )
+    def test_strange_function_multiple_cases(self):
+    
+        test_cases = [
+            ((1, 1, 0, 3), 1),
+            ((1, 1, 3, 0), 2),
+            ((0, 1, 1, 0), 3),
+            ((1, 3, 0, 0), 4),
+            ((1, 0, 0, 3), 5),
+            ((3, 0, 3, 3), 6),
+        ]
 
-    # TODO: Can you write more test cases below to increase the test coverage of `strange_function`?
+        for inputs, expected in test_cases:
+            with self.subTest(inputs=inputs):
+                result = strange_function(*inputs)
+                self.assertEqual(result, f"behaviour {expected}")
